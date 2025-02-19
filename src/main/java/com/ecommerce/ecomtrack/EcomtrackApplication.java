e10e8c99efe7d26a9389a515fbeb7839a2ba05a8
Corleone667 <talla.diop@efrei.net>
2025-02-19 15:48:19 +0100

    Initial setup

---
+++ src/main/java/com/ecommerce/ecomtrack/EcomtrackApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecomtrack/EcomtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
